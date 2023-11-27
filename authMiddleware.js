2fa1862f41db7a866bba8d08fe71a5d8455efbac
constanzaMira <cmira@correo.um.edu.uy>
2023-11-27 13:14:39 -0300

    detalles

--- authMiddleware.js
+++ authMiddleware.js
@@ -5,7 +5,7 @@ function authenticateToken(req, res, next) {
     const token = req.headers['authorization']?.split(' ')[1];
 
     if (token == null) return res.sendStatus(401);
-    if (tokenList.includes(token)) return res.sendStatus(403); // Token en lista de bloqueo
+    if (tokenList.includes(token)) return res.sendStatus(403); 
 
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
         if (err) return res.sendStatus(403);
